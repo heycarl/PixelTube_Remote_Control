160855d6987e93a5e89feb516693b4c6c1217765
Alexandr Efimchik <efimchiksasha@gmail.com>
2021-01-09 12:30:38 +0300

    The battle begins

--- Tubes_App/App.js
+++ Tubes_App/App.js
@@ -1,12 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Devices from './components/Devices';
+
+import Header from './components/Header'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header name="Sasha"/>
+      <Devices/>
+      <StatusBar style="auto" />  
     </View>
   );
 }
---
+++ Tubes_App/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+function Header(props) {
+    return (
+        <Text>
+           Header
+        </Text>
+    )
+}
+
+export default Header;
